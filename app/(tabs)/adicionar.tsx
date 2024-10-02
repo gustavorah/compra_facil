6d86d81a776c9120f5795926633111ee3293b8c1
Gustavo Rahmeier <90331005+gustavorah@users.noreply.github.com>
2024-10-02 19:29:26 -0300

    carrinho e adicionar rotas

--- app/(tabs)/adicionar.tsx
+++ app/(tabs)/adicionar.tsx
@@ -1,8 +1,9 @@
 import { View, Text, TouchableOpacity, FlatList } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { styled } from "nativewind";
 import Microphone from "@/components/Microphone";
 import * as Speech from 'expo-speech';
+import { useFocusEffect } from "@react-navigation/native";
 
 const Button = styled(TouchableOpacity);
 
@@ -17,22 +18,24 @@ const Adicionar = () => {
     const [selectedItemId, setSelectedItemId] = useState<number | null>(null); 
     const [carrinho, setCarrinho] = useState<Produto[]>([]); // Estado para armazenar os produtos no carrinho
 
-    useEffect(() => {
-        const fetchProduto = async () => {
-            try {
-                const response = await fetch('http://192.168.0.104:3000/products'); // Substitua pela URL correta
-                if (!response.ok) {
-                    throw new Error('Erro ao buscar produtos do carrinho');
-                }
-                const data = await response.json();
-                setProduto(data); // Supondo que a resposta seja um array de CarrinhoItem
-            } catch (error) {
-                console.error(error);
+    const fetchProduto = async () => {
+        try {
+            const response = await fetch('http://179.190.66.110:3000/products'); // Substitua pela URL correta
+            if (!response.ok) {
+                throw new Error('Erro ao buscar produtos do carrinho');
             }
-        };
+            const data = await response.json();
+            setProduto(data); // Supondo que a resposta seja um array de CarrinhoItem
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-        fetchProduto();
-    }, []);
+    useFocusEffect(
+        useCallback(() => {
+            fetchProduto();
+        }, [])
+    );
 
     const selectProduct = () => {
         if (selectedItemId === null) {
@@ -45,11 +48,35 @@ const Adicionar = () => {
         }
     };
 
+    const adicionarProdutoAoCarrinho = async (produto: { quantity: number, productId: number, shoppingCartId: number }) => {
+        try {
+            const response = await fetch('http://179.190.66.110:3000/shopping-cart-products', {
+                method: 'POST', // Método POST para enviar dados
+                headers: {
+                    'Content-Type': 'application/json', // Define o tipo de conteúdo como JSON
+                },
+                body: JSON.stringify(produto), // Envia os dados do produto no corpo da requisição
+            });
+    
+            if (!response.ok) {
+                throw new Error('Erro ao adicionar produto ao carrinho');
+            }
+    
+            const data = await response.json();
+            console.log('Produto adicionado com sucesso:', data);
+            return data;
+        } catch (error) {
+            console.error('Erro na requisição:', error);
+        }
+    };
+    
+
     const adicionarProduto = () => {
         if (selectedItemId !== null) {
             const produtoSelecionado = produto.find(item => item.id === selectedItemId);
             if (produtoSelecionado) {
                 setCarrinho((prevCarrinho) => [...prevCarrinho, produtoSelecionado]);
+                adicionarProdutoAoCarrinho({quantity: 1, productId: selectedItemId, shoppingCartId: 1});
                 console.log(`Produto adicionado ao carrinho: ${produtoSelecionado.name}`);
             }
         }
@@ -77,8 +104,6 @@ const Adicionar = () => {
                     </View>
                 ))}
             </View>
-
-            {/* <Microphone /> */}
 
             <View className="flex-row justify-around items-center flex-1">
                 <View className="w-80 flex-row justify-between">
--- app/(tabs)/carrinho.tsx
+++ app/(tabs)/carrinho.tsx
@@ -1,51 +1,93 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { styled } from "nativewind";
+import { useFocusEffect } from "@react-navigation/native";
 
 const Button = styled(TouchableOpacity);
 
+interface Product {
+    id: number;
+    name: string;
+    price: string;
+}
+
 interface CarrinhoItem {
     id: number;
-    nome: string;
-    quantidade: number;
+    quantity: number;
+    product: Product;
+    productId: number;
+    shoppingCartId: number;
+    createAt: string;
+    updatedAt: string;
 }
 
 const Carrinho = () => {
     const [carrinho, setCarrinho] = useState<CarrinhoItem[]>([]);
-    const [selectedItemId, setSelectedItemId] = useState<number | null>(null); 
-    const refs = useRef<{[key: number]: any}>({});
+    const [produtos, setProdutos] = useState<{[key: number]: Product}>({});
+    const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
+    const refs = useRef<{ [key: number]: any }>({});
 
-    useEffect(() => {
-        const fetchCarrinho = async () => {
-            try {
-                const response = await fetch('http://192.168.0.104:3000/shopping-cart-products'); // Substitua pela URL correta
-                if (!response.ok) {
-                    throw new Error('Erro ao buscar produtos do carrinho');
-                }
-                const data = await response.json();
-                setCarrinho(data); // Supondo que a resposta seja um array de CarrinhoItem
-            } catch (error) {
-                console.error(error);
+    const fetchCarrinho = async () => {
+        try {
+            const response = await fetch('http://179.190.66.110:3000/shopping-cart-products');
+            if (!response.ok) {
+                throw new Error('Erro ao buscar produtos do carrinho');
             }
-        };
+    
+            const data: CarrinhoItem[] = await response.json();
+            if (!data) {
+                throw new Error('Dados do carrinho não encontrados');
+            }
+    
+            setCarrinho(data);
+            data.forEach(async (item) => {
+                await fetchProduto(item.productId);
+            });
+        } catch (error: any) {
+            console.error("Erro na requisição:", error.message); // Melhor mensagem de erro
+            alert("Não foi possível carregar os itens do carrinho. Verifique sua conexão ou tente novamente mais tarde.");
+        }
+    };
+    
+    const fetchProduto = async (productId: number) => {
+        try {
+            console.log(productId);
+            const response = await fetch(`http://179.190.66.110:3000/products/${productId}`);
+            console.log(response);
+            if (!response.ok) {
+                throw new Error("Erro ao buscar o produto");
+            }
+    
+            const data: Product = await response.json();
+            if (!data) {
+                throw new Error('Dados do produto não encontrados');
+            }
 
-        fetchCarrinho();
-    }, []);
+            setProdutos(prevState => ({ ...prevState, [productId]: data }));
+        } catch (error: any) {
+            console.error("Erro ao buscar o produto:", error.message);
+        }
+    };
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchCarrinho();
+        }, [])
+    );
 
     const selectProduct = () => {
-        if (selectedItemId === null) {
+        if (selectedItemId === null && carrinho.length > 0) {
             setSelectedItemId(carrinho[0]?.id || null);
         } else {
             const currentIndex = carrinho.findIndex((item) => item.id === selectedItemId);
             const nextIndex = (currentIndex + 1) % carrinho.length;
-
             setSelectedItemId(carrinho[nextIndex].id);
         }
     };
 
     const removeProduct = () => {
         if (selectedItemId !== null) {
-            setCarrinho((prevCarrinho) => 
+            setCarrinho((prevCarrinho) =>
                 prevCarrinho.filter((item) => item.id !== selectedItemId)
             );
             setSelectedItemId(null);
@@ -55,34 +97,47 @@ const Carrinho = () => {
     return (
         <>
             <View className="flex-1 items-center justify-center">
-                {carrinho.map((item) => (
-                    <View
-                        ref={(el) => (refs.current[item.id] = el)}
-                        key={item.id}
-                        className={`border-solid border-2 m-2 w-40 rounded-lg pl-2 ${selectedItemId === item.id ? 'border-blue-500' : ''}`}
-                    >
-                        <Text>{item.nome}</Text>
-                        <Text>Quantidade: {item.quantidade}</Text>
-                    </View>
-                ))}
+                {carrinho.length > 0 ? (
+                    carrinho.map((item) => {
+                        const produto = produtos[item.productId];
+                        // console.log(produtos[item.productId]);
+                        return produto ? (
+                            <View
+                                ref={(el) => (refs.current[item.id] = el)}
+                                key={item.id}
+                                className={`border-solid border-2 m-2 w-40 rounded-lg pl-2 ${selectedItemId === item.id ? 'border-blue-500' : ''}`}
+                            >
+                                <Text>Produto: {produto.name}</Text>
+                                <Text>Preço: {produto.price}</Text>
+                                <Text>Quantidade: {item.quantity}</Text>
+                            </View>
+                        ) : (
+                            <Text key={item.id}>Carregando produto...</Text>
+                        );
+                    })
+                ) : (
+                    <Text>Carrinho Vazio</Text>
+                )}
             </View>
 
-            <View className="flex-row justify-around items-center flex-1">
-                <View className="w-80 flex-row justify-between">
-                    <Button
-                        className="bg-blue-500 rounded-lg p-10"
-                        onPress={selectProduct}
-                    >
-                        <Text className="text-white text-center">Selecionar</Text>
-                    </Button>
-                    <Button
-                        className="bg-blue-500 rounded-lg p-10"
-                        onPress={removeProduct}
-                    >
-                        <Text className="text-white text-center">Remover</Text>
-                    </Button>
+            {carrinho.length > 0 && (
+                <View className="flex-row justify-around items-center flex-1">
+                    <View className="w-80 flex-row justify-between">
+                        <Button
+                            className="bg-blue-500 rounded-lg p-10"
+                            onPress={selectProduct}
+                        >
+                            <Text className="text-white text-center">Selecionar</Text>
+                        </Button>
+                        <Button
+                            className="bg-blue-500 rounded-lg p-10"
+                            onPress={removeProduct}
+                        >
+                            <Text className="text-white text-center">Remover</Text>
+                        </Button>
+                    </View>
                 </View>
-            </View>
+            )}
         </>
     );
 };
