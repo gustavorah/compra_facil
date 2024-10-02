56022176e479eb41590bc727f34f3f501f7af91a
Gustavo Rahmeier <90331005+gustavorah@users.noreply.github.com>
2024-10-01 22:31:48 -0300

    bla

--- app/(tabs)/adicionar.tsx
+++ app/(tabs)/adicionar.tsx
@@ -14,6 +14,8 @@ interface Produto {
 
 const adicionar = () => {
     const [produto, setProduto] = useState<Produto[]>([]);
+    const [selectedItemId, setSelectedItemId] = useState<number | null>(null); 
+
     useEffect(() => {
         setProduto([
         {
@@ -24,41 +26,71 @@ const adicionar = () => {
         ]);
     }, []);
 
+    const selectProduct = () => {
+        if (selectedItemId === null) {
+            setSelectedItemId(produto[0]?.id || null);
+        }
+        else {
+            const currentIndex = produto.findIndex((item) => item.id == selectedItemId);
+            const nextIndex = (currentIndex + 1) % produto.length;
+
+            setSelectedItemId(produto[nextIndex].id);
+        }
+    };
+
+    const adicionarProduto = () => {
+        if (selectedItemId !== null) {
+            
+        }
+    }
+
     const descricaoProduto = (item: Produto) => {
         const descricaoItem = `Produto selecionado: ${item.nome}, preço de ${item.preco} reais`;
         Speech.speak(descricaoItem, {language: "pt-br"});
     }
 
-  return (
+    return (
         <>
-        <View className="flex-1 justify-center items-center">
-            <Text>Informações sobre o produto</Text>
-            {produto.map((item) => (
-            <View
-                key={item.id}
-                className="border-solid border-2 m-2 w-40 rounded-lg pl-2"
-            >
-                <Text>Nome: {item.nome}</Text>
-                <Text>Preco: {item.preco}</Text>
-                <Button className="bg-blue-500 rounded-lg mt-2 mb-2 mr-2" onPress={() => descricaoProduto(item)}>
-                    <Text className="text-white text-center">Saber mais</Text>
-                </Button>
+            <View className="flex-1 justify-center items-center">
+                <Text>Informações sobre o produto</Text>
+                {produto.map((item) => (
+                <View
+                    key={item.id}
+                    className={`border-solid border-2 m-2 w-40 rounded-lg pl-2 ${selectedItemId == item.id ? 'border-blue-500' : ''}`}
+                >
+                    <Text>Nome: {item.nome}</Text>
+                    <Text>Preco: {item.preco}</Text>
+                    <Button className="bg-blue-500 rounded-lg mt-2 mb-2 mr-2" onPress={() => descricaoProduto(item)}>
+                        <Text className="text-white text-center">Saber mais</Text>
+                    </Button>
+                </View>
+                ))}
+
             </View>
-            ))}
 
-        </View>
+            <Microphone 
 
-        <Microphone 
+            />
 
-        />
+            <View className="flex-row justify-around items-center flex-1">
 
-        <View className="flex-1 justify-center items-center">
-            <View>
-            <Button className="bg-blue-500 w-40 p-10 rounded-lg">
-                <Text className="text-white text-center">Adicionar</Text>
-            </Button>
+
+                <View className="w-80 flex-row justify-between">
+                    <Button 
+                        className="bg-blue-500 w-40 p-10 rounded-lg"
+                        onPress={adicionarProduto}    
+                    >
+                        <Text className="text-white text-center">Adicionar</Text>
+                    </Button>
+
+                    <Button
+                        className="bg-blue-500 rounded-lg p-10"
+                        onPress={selectProduct}
+                    >
+                    <Text className="text-white text-center">Selecionar</Text>
+                    </Button>
+                </View>
             </View>
-        </View>
         </>
     );
 };
--- app/(tabs)/carrinho.tsx
+++ app/(tabs)/carrinho.tsx
@@ -1,56 +1,85 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { styled } from "nativewind";
 
 const Button = styled(TouchableOpacity);
 
 interface CarrinhoItem {
-  id: number;
-  nome: string;
-  quantidade: number;
+    id: number;
+    nome: string;
+    quantidade: number;
 }
 
 const Carrinho = () => {
-  const [carrinho, setCarrinho] = useState<CarrinhoItem[]>([]);
-  const [textButton, setTextButton] = useState("Selecionar");
-  useEffect(() => {
-    // Simulando uma adição de itens ao carrinho
-    setCarrinho([
-      { id: 1, nome: "Compra 1", quantidade: 2 },
-      { id: 2, nome: "Compra 2", quantidade: 1 },
-      { id: 3, nome: "Compra 3", quantidade: 5 },
-    ]);
-  }, []);
+    const [carrinho, setCarrinho] = useState<CarrinhoItem[]>([]);
+    const [textButton, setTextButton] = useState("Selecionar");
+    const [selectedItemId, setSelectedItemId] = useState<number | null>(null); 
 
-  const changeButton = () => {
-    setTextButton(textButton === "Selecionar" ? "Remover" : "Selecionar");
-  };
-  return (
-    <>
-      <View className="flex-1 items-center justify-center">
-        {carrinho.map((item) => (
-          <View
-            key={item.id}
-            className="border-solid border-2 m-2 w-40 rounded-lg pl-2"
-          >
-            <Text>{item.nome}</Text>
-            <Text>Quantidade: {item.quantidade}</Text>
-          </View>
-        ))}
-      </View>
+    useEffect(() => {
+        // Simulando uma adição de itens ao carrinho
+        setCarrinho([
+        { id: 1, nome: "Compra 1", quantidade: 2 },
+        { id: 2, nome: "Compra 2", quantidade: 1 },
+        { id: 3, nome: "Compra 3", quantidade: 5 },
+        ]);
+    }, []);
+    
+    const selectProduct = () => {
+        if (selectedItemId === null) {
+            setSelectedItemId(carrinho[0]?.id || null);
+        }
+        else {
+            const currentIndex = carrinho.findIndex((item) => item.id == selectedItemId);
+            const nextIndex = (currentIndex + 1) % carrinho.length;
 
-      <View className="flex-row justify-around items-center flex-1">
-        <View className="w-40 ">
-          <Button
-            className="bg-blue-500 rounded-lg p-10"
-            onPress={changeButton}
-          >
-            <Text className="text-white text-center">{textButton}</Text>
-          </Button>
-        </View>
-      </View>
-    </>
-  );
+            setSelectedItemId(carrinho[nextIndex].id);
+        }
+    };
+
+    const removeProduct = () => {
+        if (selectedItemId !== null) {
+            setCarrinho((prevCarrinho) => 
+                prevCarrinho.filter((item) => item.id !== selectedItemId)
+            );
+            setSelectedItemId(null);
+        }
+    };
+
+    const refs = useRef<{[key: number]: any}>({});
+
+    return (
+        <>
+            <View className="flex-1 items-center justify-center">
+                {carrinho.map((item) => (
+                <View
+                    ref={(el) => (refs.current[item.id] = el)}
+                    key={item.id}
+                    className={`border-solid border-2 m-2 w-40 rounded-lg pl-2 ${selectedItemId == item.id ? 'border-blue-500' : ''}`}
+                >
+                    <Text>{item.nome}</Text>
+                    <Text>Quantidade: {item.quantidade}</Text>
+                </View>
+                ))}
+            </View>
+
+            <View className="flex-row justify-around items-center flex-1">
+                <View className="w-80 flex-row justify-between">
+                <Button
+                    className="bg-blue-500 rounded-lg p-10"
+                    onPress={selectProduct}
+                >
+                    <Text className="text-white text-center">Selecionar</Text>
+                </Button>
+                <Button
+                    className="bg-blue-500 rounded-lg p-10"
+                    onPress={removeProduct}
+                >
+                    <Text className="text-white text-center">Remover</Text>
+                </Button>
+                </View>
+            </View>
+        </>
+    );
 };
 
 export default Carrinho;
